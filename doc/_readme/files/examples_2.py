0adb3a565c6c0d7fba3f1781b4e76c8ce06b73bc
psudo-dev <renatosetoue@gmail.com>
2026-01-25 17:06:34 -0300

    v.2.2.3
    uploading README new example images

---
+++ doc/_readme/files/examples_2.py
@@ -0,0 +1,20 @@
+class Animal:
+    def __init__(self, name: str):
+        self.name = name
+
+    def move(self, distance_in_meters: int = 0):
+        print(f"{self.name} moved {distance_in_meters}m.")
+
+class Raven(Animal):
+    def __init__(self, name: str):
+        # In Python, we call the parent constructor using super()
+        super().__init__(name)
+
+    def move(self, distance_in_meters: int = 100):
+        print("Playing around...")
+        # Accessing the parent's method logic
+        super().move(distance_in_meters)
+
+# Example Usage:
+my_raven = Raven("Nevermore")
+my_raven.move() # Output: Playing around... Nevermore moved 100m.
