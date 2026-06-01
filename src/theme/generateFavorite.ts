4f071f9c0e856448ec60863fb6ae3929fbc15e6c
Renato Setoue <renatosetoue@gmail.com>
2026-06-01 17:55:49 -0300

    added generateFavorite

---
+++ src/theme/generateFavorite.ts
@@ -0,0 +1,20 @@
+import fs from "node:fs";
+import path from "node:path";
+import { ThemeSettings, SyntaxNames } from "../types/settings";
+import { buildTheme } from "./buildTheme";
+
+export const generateFavorite = (settings: ThemeSettings): void => {
+	const syntaxName = settings.favorite.match(
+		/\(([a-z]+)\)/i,
+	)?.[1] as SyntaxNames;
+	const theme = buildTheme(settings, syntaxName);
+	const themesDirPath = path.join(__dirname, "../themes/");
+	const fileName = "nebula-oni-theme-favorite";
+	const message = `${fileName} file has been created!`;
+	try {
+		fs.writeFileSync(`${themesDirPath}${fileName}`, JSON.stringify(theme));
+		console.log(message);
+	} catch (err) {
+		console.error(`Failed to write ${fileName}:`, err);
+	}
+};
