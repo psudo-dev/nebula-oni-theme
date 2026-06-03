e544fffdc595d1c72ceaba6cac4ecc56aa1e224a
Renato Setoue <renatosetoue@gmail.com>
2026-06-03 17:11:06 -0300

    extension activation file

--- src/extension/main.ts
+++ src/extension/main.ts
@@ -1,26 +1,58 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import {
+	getApplyChanges,
+	getRestoreConfig,
+	restoreMenuSettings,
+	saveRestoreConfig,
+} from "../vscodeAPI/settings";
+import {
+	defaultFavoriteSettings,
+	defaultThemeSettings,
+} from "../vscodeAPI/defaults";
+import {
+	showInstallMessage,
+	showUpdateMessage,
+} from "../vscodeAPI/information";
+import {
+	installMessage,
+	psudoFontOption,
+	updateMessage,
+} from "../vscodeAPI/messages";
+import { generateThemes } from "../theme/generateThemes";
+import { generateFavorite } from "../theme/generateFavorite";
+import { applyChanges } from "../vscodeAPI/applyChanges";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const currentVersion = context.extension.packageJSON.version;
+	const storedVersion = context.globalState.get<string>("version");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "nebula-oni-refactor" is now active!');
+	if (storedVersion === undefined) {
+		saveRestoreConfig(defaultThemeSettings, defaultFavoriteSettings);
+		showInstallMessage(installMessage, psudoFontOption);
+		context.globalState.update("version", currentVersion);
+	} else if (storedVersion !== currentVersion) {
+		const { themeSettings, favoriteSettings } = getRestoreConfig();
+		generateThemes(themeSettings);
+		generateFavorite(favoriteSettings);
+		restoreMenuSettings(themeSettings);
+		showUpdateMessage(updateMessage, psudoFontOption);
+		context.globalState.update("version", currentVersion);
+	} else {
+		const { themeSettings } = getRestoreConfig();
+		restoreMenuSettings(themeSettings);
+	}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('nebula-oni-refactor.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from nebula-oni-refactor!');
+	const listener = vscode.workspace.onDidChangeConfiguration((event) => {
+		if (!event.affectsConfiguration("nebulaOni")) {
+			return;
+		}
+		if (!getApplyChanges()) {
+			return;
+		}
+		applyChanges();
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(listener);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
