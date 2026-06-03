3042e6b2ffec82de41deeff644844d4be0a90120
Renato Setoue <renatosetoue@gmail.com>
2026-06-03 17:09:53 -0300

    theme defaults

---
+++ src/vscodeAPI/defaults.ts
@@ -0,0 +1,23 @@
+import { ThemeSettings } from "../types/settings";
+
+export const defaultThemeSettings: ThemeSettings = {
+	background: "Grey",
+	main: "Bumblebee",
+	highlight: "Ultra Violet",
+	focusBorder: "ON",
+	fontStyle: "Italic",
+	syntaxBrightness: "Normal",
+	comments: "Dark Violet",
+	favorite: "OFF",
+};
+
+export const defaultFavoriteSettings: ThemeSettings = {
+	background: "Glacial Blue",
+	main: "Sakura",
+	highlight: "Ultra Violet",
+	focusBorder: "ON",
+	fontStyle: "Italic",
+	syntaxBrightness: "Dimmed",
+	comments: "Dark Magenta",
+	favorite: "Nebula Oni (Pegasus)",
+};
